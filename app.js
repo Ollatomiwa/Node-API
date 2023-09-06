400101bc0fb1d210ec1e2194c33e3dc7f7c837fe
ollatomiwa <ajaomoshood@gmail.com>
2023-09-06 21:41:54 +0100

    new event created with post http

--- app.js
+++ app.js
@@ -1,16 +1,48 @@
 const express = require ('express');
+const fs = require ('fs');
 
 const app = express();
 
-app.get('/', (req,res) =>{
-    res.json({message:'Hello from this server...', app: 'ComputAPI'});
+// app.get('/', (req,res) =>{
 
+
+//     res.json({message:'Hello from this server...', app: 'ComputAPI'});
+
+// });
+
+// app.post('/', (req, res) =>{
+//     res.send(`you can continue your post.....`);
+// });
+
+app.use(express.json());
+
+const tours = JSON.parse( fs.readFileSync(`${__dirname}/dev_data/data/tours-simple.json`));
+
+
+app.get (`api/v1/tours`, (req, res) => {
+        res.status(200) .json({
+            status: "suceess",
+            result: tours.length,
+            data: {
+                tours
+            }
+        });
+    });
+
+app.post (`api/v1/tours`, (req, res) => {
+
+    const newId = tours[tours.length -1] .id +1;
+    const newTour = Object.assign({id: newId}, req.body);
+    tours.push = newTour;
+    fs.writeFile (`${__dirname}/dev_data/data/tours-simple.json`), JSON.stringify(tours), err => {
+        res.status(201) .json({
+            status: 'success',
+            data: {
+                tours: newTour
+            }
+        });
+    };
 });
-
-app.post('/', (req, res) =>{
-    res.send(`you can continue your post.....`);
-});
-
 const port = 3000;
 
 app.listen( port, ()=>{
