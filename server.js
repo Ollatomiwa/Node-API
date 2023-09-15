4ce9ff855445155444d0c5260e5f10009c39866c
ollatomiwa <ajaomoshood@gmail.com>
2023-09-15 20:55:30 +0100

    mongoDB connected to Express

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({path: './config.env'});
 const app = require ('./app'); //importing the app.js to use in the server.js
 
+dotenv.config({path: './config.env'});
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+} ) .then(() => console.log('DB connction succesful'));
+ 
 //console.log(process.env);
 const port = process.env.PORT || 3000;
+
 
 app.listen( port, ()=>{
     console.log(`App running on port ${port}......`);
